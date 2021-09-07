b8aa2f843150cc1d11e3e4c171a0296e833c4949
Alex Wintle <alexwintle663@gmail.com>
2021-09-07 13:04:29 +0100

    refactored code to use array.length in output

--- src/directory.rb
+++ src/directory.rb
@@ -1,19 +1,19 @@
-puts "The students of Villains Academy"
-puts "-------------"
-puts 'Deadpool'
-puts 'Bruce'
-puts 'Hans Gruber'
-puts 'Immortan Joe'
-puts 'Dr. Hannibal Lecter'
-puts 'Michael Myers'
-puts 'Darth Vader'
-puts 'Michael Corleone'
-puts 'The Wicked Witch of the West'
-puts 'Terminator'
-puts 'Freddy Krueger'
-puts 'The Joker'
-puts 'Joffrey Baratheon'
-print "Overall we have "
-print 13
-print " great students!"
+# frozen_string_literal: true
 
+students = [
+  'Deadpool',
+  'Bruce',
+  'Hans Gruber',
+  'Immortan Joe',
+  'Dr. Hannibal Lecter',
+  'Michael Myers',
+  'Darth Vader',
+  'Michael Corleone',
+  'The Wicked Witch of the West',
+  'Terminator',
+  'Freddy Krueger',
+  'The Joker',
+  'Joffrey Baratheon'
+]
+
+print "Overall we have #{students.length} great students"
