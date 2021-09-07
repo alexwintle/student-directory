2cb70000e15f35c32d0070fbfe82f4d02ca59613
Alex Wintle <alexwintle663@gmail.com>
2021-09-07 12:44:29 +0100

    first commit

---
+++ directory.rb
@@ -0,0 +1,19 @@
+puts "The students of Villains Academy"
+puts "-------------"
+puts 'Deadpool'
+puts 'Bruce'
+puts 'Hans Gruber'
+puts 'Immortan Joe'
+puts 'Dr. Hannibal Lecter'
+puts 'Michael Myers'
+puts 'Darth Vader'
+puts 'Michael Corleone'
+puts 'The Wicked Witch of the West'
+puts 'Terminator'
+puts 'Freddy Krueger'
+puts 'The Joker'
+puts 'Joffrey Baratheon'
+print "Overall we have "
+print 13
+print " great students!"
+
